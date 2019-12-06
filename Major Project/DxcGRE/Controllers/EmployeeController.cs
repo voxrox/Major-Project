89bcff5d409e4c1cdde7cf3b8529b1321b7dbf76
ycdxc <55533556+ycdxc@users.noreply.github.com>
2019-12-06 17:40:29 +0530

    GRE Portal

---
+++ Major Project/DxcGRE/Controllers/EmployeeController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DxcGRE.Models;
+using System.Web.Security;
+
+namespace DxcGRE.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Major_ProjectEntities context = new Major_ProjectEntities();
+        static int ID;
+        // GET: Employee
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(employee login)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isValid = context.employees.Any(x => x.Email == login.Email && x.Password == login.Password);
+                if (isValid)
+                {
+                    employee data = context.employees.FirstOrDefault(x => x.Email == login.Email);
+                    ID = (int)data.EmpID;
+                    FormsAuthentication.SetAuthCookie(data.EmpName, false);
+                    return RedirectToAction("EmployeeDashboard");
+                }
+                ViewBag.Error = "Invalid User ID or Password";
+                return View();
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult EmployeeDashboard()
+        {
+            return View();
+        }
+
+        public ActionResult TrainingRooms()
+        {
+            ViewBag.Rooms = Rooms();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TrainingRooms(dashboard data, FormCollection form)
+        {
+            ViewBag.Rooms = Rooms();
+            data.EmpID = ID;
+            data.BookingStatus = "Pending";
+            data.BookingType = "Training Room";
+            int daysdifference = ((TimeSpan)(data.DateTo - data.DateFrom)).Days;
+            if (data.DateTo >= data.DateFrom && daysdifference <= 93)
+            {
+                bool svalid = context.dashboards.Any(x => x.RoomName == data.RoomName);
+                if (svalid)
+                {
+                    if (data.DateFrom == data.DateTo)
+                    {
+                        bool evalid = context.dashboards.Any(x => x.DateFrom == data.DateFrom);
+                        if (!evalid)
+                        {
+
+                            context.dashboards.Add(data);
+                            context.SaveChanges();
+                            return RedirectToAction("EmployeeDashboard");
+                        }
+                    }
+
+                    bool zcalid = context.dashboards.Any(x => x.DateFrom == data.DateFrom || x.DateFrom == data.DateTo || x.DateTo == data.DateFrom || x.DateTo == data.DateTo);
+                    if (zcalid)
+                    {
+                        ModelState.AddModelError("RoomName", " training room  " + data.RoomName + " has already been booked on " + data.DateFrom + " or " + data.DateTo);
+                    }
+                    else
+                    {
+                        bool fvalid = context.dashboards.Any(x => x.DateFrom < data.DateFrom && x.DateTo > data.DateFrom || x.DateFrom < data.DateTo && x.DateTo > data.DateTo);
+                        bool dvalid = context.dashboards.Any(x => x.DateFrom > data.DateFrom && x.DateFrom < data.DateTo && x.DateTo > data.DateFrom && x.DateTo < data.DateTo);
+                        if (dvalid)
+                        {
+                            ModelState.AddModelError("RoomName", "Training Room "+data.RoomName+"already been booked in between the given dates");
+                            return View();
+
+                        }
+                        else if (fvalid)
+                        {
+                            ModelState.AddModelError("RoomName", "Training Room " + data.RoomName + "already been booked in between the given dates");
+                        }
+                        else
+                        {
+                            context.dashboards.Add(data);
+                            context.SaveChanges();
+                            return RedirectToAction("EmployeeDashboard");
+                        }
+                    }
+                }
+                else
+                {
+                    context.dashboards.Add(data);
+                    context.SaveChanges();
+                    return RedirectToAction("EmployeeDashboard");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("RoomName", "*Invalid Dates OR Booking Period Must Be Less Than 3 Months");
+            }
+            return View();
+
+
+        }
+
+        public ActionResult CleaningServices()
+        {
+            ViewBag.Rooms = Rooms();
+            ViewBag.ServiceType = ServiceType();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CleaningServices(dashboard data, FormCollection form)
+        {
+            ViewBag.Rooms = Rooms();
+            ViewBag.ServiceType = ServiceType();
+            dashboard dbdata = new dashboard();
+            dbdata.EmpID = ID;
+            dbdata.RoomName = form["RoomName"].ToString();
+            dbdata.BookingType = form["BookingType"].ToString();
+            dbdata.BookingStatus = "Pending";
+            dbdata.DateTo = data.DateFrom;
+            dbdata.DateFrom = data.DateFrom;
+            context.dashboards.Add(dbdata);
+            context.SaveChanges();
+            return RedirectToAction("EmployeeDashboard");
+        }
+
+        public ActionResult ServicesBooked()
+        {
+
+            var BookedServices = context.dashboards.Where(x => x.EmpID == ID);
+            return View(BookedServices);
+        }
+
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Feedback(feedback fback, FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                fback.Empid = ID;
+                fback.Usability = form[0];
+                fback.Service = form[1];
+                fback.User_Experience = form[2];
+               
+                context.feedbacks.Add(fback);
+                context.SaveChanges();
+                return RedirectToAction("FeedbackResponse");
+               
+            }
+            return View();
+        }
+
+        public ActionResult FeedbackResponse()
+        {
+            return View();
+        }
+
+        public ActionResult RemoveBooking(int id)
+        {
+            dashboard data = context.dashboards.Find(id);
+            context.dashboards.Remove(data);
+            context.SaveChanges();
+            return RedirectToAction("ServicesBooked");
+        }
+
+        public ActionResult Availabilty()
+        {
+            ViewBag.Rooms = Rooms();
+            return View(); 
+        }
+
+        public ActionResult _AvailabilityPartial(string room)
+        {
+            var availabledates = context.dashboards.Where(x => x.RoomName == room);
+            ViewBag.availabledates = availabledates.ToList();
+            return PartialView("_AvailabilityPartial");
+        }
+
+        public List<string> Rooms()
+        {
+            List<string> roomnames = new List<string>() { "Purdue", "Columbia", "Oxford", "Helsinki", "Kyoto", "Yale", "Carneige mellon", "Jhon Hopkins", "Cambridge", "Berkely" };
+            return roomnames;
+        }
+        public List<string> ServiceType()
+        {
+            List<string> Servicetype = new List<string>() { "Furniture Cleaning", "Carpet Cleaning", "Glass Cleaning", "Deep Cleaning", "Room Freshener Service" };
+            return Servicetype;
+        }
+    }
+}
---
+++ Major Project/DxcGRE/Controllers/ManagerController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Security;
+using System.Web.Mvc;
+using DxcGRE.Models;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+
+namespace DxcGRE.Controllers
+{
+    public class ManagerController : Controller
+    {
+        Major_ProjectEntities context = new Major_ProjectEntities();
+        // GET: Manager
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(gre_manager login)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isValid = context.gre_manager.Any(x => x.Email == login.Email && x.Password == login.Password);
+                
+                if (isValid)
+                {
+                    gre_manager data = context.gre_manager.FirstOrDefault(x=>x.Email==login.Email);
+                    FormsAuthentication.SetAuthCookie(data.ManagerName, false);
+                    return RedirectToAction("Dashboard");
+                }
+                ViewBag.Error = "Invalid User ID or Password";
+                return View();
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult Dashboard()
+        {
+            var BookedServices = context.dashboards.ToList();
+            BookedServices.Reverse();
+            return View(BookedServices);
+        }
+
+        [Authorize]
+        public ActionResult Feedback()
+        {
+            var Feedbacks = context.feedbacks.ToList();
+            Feedbacks.Reverse();
+            return View(Feedbacks);
+        }
+
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+
+            // clear authentication cookie
+            HttpCookie cookie1 = new HttpCookie(FormsAuthentication.FormsCookieName, "");
+            cookie1.Expires = DateTime.Now.AddYears(-1);
+            Response.Cookies.Add(cookie1);
+            return Redirect("~/Home/Index");
+        }
+
+        public ActionResult Approve(int id)
+        {
+            dashboard data = context.dashboards.Find(id);
+            data.BookingStatus = "Approved";
+            int ID = (int)data.EmpID;
+            var employee = context.employees.FirstOrDefault(x=>x.EmpID==ID);
+            var Email = employee.Email;
+            MailMessage mailcontext = new MailMessage("dxcgre@gmail.com", Email);
+            mailcontext.Subject = "Booking Status";
+            mailcontext.Body = "Your Booking has benn confirmed";
+            mailcontext.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("dxcgre@gmail.com", "DXCGREADMIN");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mailcontext);
+            context.SaveChanges();
+            return RedirectToAction("Dashboard");
+        }
+
+        public ActionResult Reject(int id)
+        {
+            dashboard data = context.dashboards.Find(id);
+            data.BookingStatus = "Declined";
+            int ID = (int)data.EmpID;
+            var employee = context.employees.FirstOrDefault(x => x.EmpID == ID);
+            var Email = employee.Email;
+            MailMessage mailcontext = new MailMessage("dxcgre@gmail.com", Email);
+            mailcontext.Subject = "Booking Status";
+            mailcontext.Body = "Sorry, Your Booking has benn declined!";
+            mailcontext.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("dxcgre@gmail.com", "DXCGREADMIN");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mailcontext);
+            context.SaveChanges();
+            return RedirectToAction("Dashboard");
+        }
+    }
+}
